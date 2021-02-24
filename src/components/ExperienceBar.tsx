fdd9df795531cf439a14154aa9cbd4f5c4b74831
bruno-br <bruno.brasilf@gmail.com>
2021-02-24 17:51:55 -0300

    Implemented CSS modules to the ExperienceBar component

--- src/components/ExperienceBar.tsx
+++ src/components/ExperienceBar.tsx
@@ -1,12 +1,15 @@
-import React from "react";
+import styles from "../styles/components/ExperienceBar.module.css";
 
 function ExperienceBar() {
     return (
-        <header className="experience-bar">
+        <header className={styles.experienceBar}>
             <span>0 xp</span>
             <div>
                 <div style={{ width: "60%" }}></div>
-                <span className="current-experience" style={{ left: "50%" }}>
+                <span
+                    className={styles.currentExperience}
+                    style={{ left: "50%" }}
+                >
                     300 xp
                 </span>
             </div>
