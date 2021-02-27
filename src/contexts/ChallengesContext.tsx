b901a3a6bf9991a8553095a43f5bc79c53d26280
bruno-br <bruno.brasilf@gmail.com>
2021-02-27 00:26:06 -0300

    Show notification and play sound effect when a challenge appears

--- src/contexts/ChallengesContext.tsx
+++ src/contexts/ChallengesContext.tsx
@@ -1,4 +1,4 @@
-import { createContext, useState, ReactNode } from "react";
+import { createContext, useState, ReactNode, useEffect } from "react";
 import challenges from "../../challenges.json";
 
 interface Challenge {
@@ -34,6 +34,10 @@ export function ChallengesProvider({ children }: ChallengesProvider) {
 
     const experienceToNextLevel = Math.pow((level + 1) * 4, 2);
 
+    useEffect(() => {
+        Notification.requestPermission();
+    }, []);
+
     function levelUp() {
         setLevel(level + 1);
     }
@@ -44,6 +48,13 @@ export function ChallengesProvider({ children }: ChallengesProvider) {
         );
         const challenge = challenges[randomChallengeIndex];
         setActiveChallenge(challenge);
+
+        if (Notification.permission === "granted") {
+            new Audio("./notification.mp3").play();
+            new Notification("Novo desafio!", {
+                body: `Valendo ${challenge.amount} xp!`
+            });
+        }
     }
 
     function resetChallenge() {
